ff9624afb148667d93a57a909a6467998e3ae1df
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2022-03-16 17:22:26 +0700

    2022 03 16

---
+++ datacenter_sip2/datasip2/WinFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+using datasip2.Error_provider;
+using datasip2.Isvaild_model;
+using SelfCheckHybrid.Models.Sip2;
+using System.ComponentModel.DataAnnotations;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string message = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                message = Valid_provider.Instances.isvaild(new BookInformation(), textBox1.Text, ref message).ToString();
+                MessageBox.Show("" + message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(""+Error_providers.Instances.CustomsExceptions(ex));
+            }
+          
+        }
+    }
+}
