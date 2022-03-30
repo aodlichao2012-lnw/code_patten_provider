4d2aecdfe9e84b8f97d2e1888d3325d93821d34e
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2022-03-30 17:00:56 +0700

    2022 03 30 ver8

--- DcFramework/DataCenter2/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DataCenter2
-{
-    public class Class1
-    {
-    }
-}
---
+++ DcFramework/Dc/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dc
+{
+    public class Class1
+    {
+    }
+}
