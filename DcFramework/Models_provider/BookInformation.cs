bb7e1f169bb433fb499bd660d69d9d49d46f234d
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2022-03-30 16:46:05 +0700

    2022 03 30 ver6

---
+++ DcFramework/Models_provider/BookInformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SelfCheckHybrid.Models.Sip2
+{
+    public class BookInformation
+    {
+        [RegularExpression("^[0-9]*$", ErrorMessage = "กรุณากรอกตัวเลข")]
+        [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Barcodebook")]
+        [MaxLength(50, ErrorMessage = "คุณป้อนตัวอักษรเกิน 50 อักขระ")]
+        public string Barcodebook { get; set; } = "";
+        [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Duedate", AllowEmptyStrings = false)]
+        public string Duedate { get; set; } = "";
+
+       [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Title", AllowEmptyStrings = false)]
+        public string Title { get; set; } = "";
+       [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Owner", AllowEmptyStrings = false)]
+        public string Owner { get; set; } = "";
+       [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Fee", AllowEmptyStrings = false)]
+        public string Fee { get; set; } = "";
+       [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Location", AllowEmptyStrings = false)]
+        public string Location { get; set; } = "";
+        [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Callnumber", AllowEmptyStrings = false)]
+        [MaxLength(50,ErrorMessage = "คุณป้อนตัวอักษรเกิน 50")]
+        public string Callnumber { get; set; } = "";
+       [Required(ErrorMessage = "คุณยังไม่ได่้กรอก Message", AllowEmptyStrings = false)]
+        public string Message { get; set; } = "";
+
+        public override string ToString()
+        {
+            return $"{nameof( Barcodebook).ToString()}";
+        }
+    }
+}
---
+++ DcFramework/Models_provider/UserInformation.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCheckHybrid.Models.Sip2
+{
+    public class UserInformation
+    {
+       [Required(ErrorMessage ="คุณยังไม่ได่้กรอก " , AllowEmptyStrings = false)]
+        public string PatronID { get; set; } = "";
+       [Required(ErrorMessage ="คุณยังไม่ได่้กรอก " , AllowEmptyStrings = false)]
+        public string PatronName { get; set; } = "";
+       [Required(ErrorMessage ="คุณยังไม่ได่้กรอก " , AllowEmptyStrings = false)]
+        public string Fee { get; set; } = "";
+
+
+        public override string ToString()
+        {
+            return $"{nameof(PatronID).ToString()}";
+        }
+    }
+}
